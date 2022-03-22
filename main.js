bcfd76a5f0801bcdf87b19a1c4eb57df5affac3e
“CatalinaSonora” <catalina.sonora1@gmail.com>
2022-03-22 13:16:47 -0300

    segundo commit

--- main.js
+++ main.js
@@ -41,20 +41,16 @@ $(document).ready(function () {
 })
 
 
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
+const nav = document.querySelector("#navbar");
+const NavTop = nav.offsetTop;
 
-// Get the navbar
-var navbar = document.getElementById("menu");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
+function fixnavbar(){
+  if(window.scrollY >= NavTop){
+    document.body.style.paddingTop = nav.offsetHeight + "px";
+    document.body.classList.add("fixed-nav");
+  }else {
+    document.body.style.paddingTop = 0;
+    document.body.classList.remove("fixed-nav");
   }
 }
+window.addEventListener("scroll", fixnavbar);
